79e4e8c5c63ae3cb1f6798e1dc12302ccf1335ba
Adam Keenan <adam_keenan@taylor.edu>
2013-09-28 08:07:02 +0000

    Added some nice SCSS :)

--- app/models/textbook.rb
+++ app/models/textbook.rb
@@ -4,5 +4,5 @@ class Textbook < ActiveRecord::Base
   validates :authors, :presence => true, :uniqueness => true
   validates :edition, :uniqueness => true
   validates :isbn, :presence => true, :uniqueness => true, :length => {:is => 13}
-  validates :price, :format => /\A\d+(?:\.\d{0,2})?\z/
+  validates :price, :presence => true, :format => /\A\d+(?:\.\d{0,2})?\z/
 end
