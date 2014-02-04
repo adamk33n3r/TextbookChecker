4da3d96bca11491c77bdadbf28ae4a5900da8d62
Adam Keenan <adam_keenan@taylor.edu>
2014-02-04 05:33:00 +0000

    Converted remaining references to 'user' to 'student'

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,11 @@
 class StudentsController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_student!, except: [:new, :create]
-  #before_action :ensure_correct_user, :only => [:edit, :update]
-  #before_action :ensure_admin_or_correct_user, :only => [:edit, :updat, :destroy]
 
   # GET /students
   # GET /students.json
   def index
-    @users = Student.all
+    @students = Student.all
   end
 
   # GET /students/1
@@ -21,7 +19,7 @@ class StudentsController < ApplicationController
 
   # GET /students/new
   def new
-    @user = Student.new
+    @student = Student.new
   end
 
   # GET /students/1/edit
@@ -31,21 +29,14 @@ class StudentsController < ApplicationController
   # POST /students
   # POST /students.json
   def create
-    @user = Student.new(user_params)
-    #@user.username = @user.email.split('@')[0]
-    if !Rails.env.production?
-      fail "isn't production"
-      @user.skip_confirmation!
-    else
-      fail "is production"
-    end
-    if @user.save
+    @student = Student.new(student_params)
+    if @student.save
       #UserMailer.verify_email(@user).deliver
-      flash[:success] = "Welcome, #{@user.username.titleize}!"
-      cookies[:logged] = true
-      cookies[:user] = @user.username
-      cookies.signed[:user_id] = @user.id
-      redirect_to @user
+      flash[:success] = "Welcome, #{@student.username.titleize}!"
+      #cookies[:logged] = true
+      #cookies[:user] = @student.username
+      #cookies.signed[:user_id] = @user.id
+      redirect_to @student
     else
       render action: 'new'
     end
@@ -55,12 +46,12 @@ class StudentsController < ApplicationController
   # PATCH/PUT /students/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'Student was successfully updated.' }
+      if @student.update(student_params)
+        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -68,7 +59,7 @@ class StudentsController < ApplicationController
   # DELETE /students/1
   # DELETE /students/1.json
   def destroy
-    @user.destroy
+    @student.destroy
     respond_to do |format|
       format.html { redirect_to students_url }
       format.json { head :no_content }
@@ -77,12 +68,12 @@ class StudentsController < ApplicationController
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = Student.find_by_username(params[:id])
+  def set_student
+    @student = Student.find_by_username(params[:id])
   end
   
   # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
+  def student_params
     params.require(:student).permit(:email, :password, :password_confirmation, :year)
   end
 end
